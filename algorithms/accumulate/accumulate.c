bac23936838c203b5b1baeed19ebbdd82c0eb9f3
William J Borom <boromwilliam@gmail.com>
2020-07-20 14:13:35 -0400

    commting

--- algorithms/accumulate/accumulate.c
+++ algorithms/accumulate/accumulate.c
@@ -10,5 +10,14 @@ int accumulate(
     int start, 
     binary_op_f op)
 {
-    return 0;
+    if (last <= start) {
+        return 0;
+    }
+    int i;
+    size_t size = sizeof(int);
+    int accumulator = start;
+    for (i = 0; first + size * i <= last; i++) {
+        accumulator = op(accumulator, array[i]);
+    }
+    return accumulator;
 }
